06d1071c463f3bb5c59dfa6183aa635ed36059a5
brisequentin <brise.quentin@gmail.com>
2022-02-09 10:16:26 +0100

    Upload session dexo 1

---
+++ Niveau 1/1/2-presentation.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+       System.out.println("Coucou !");
+       System.out.println("Je m'appelle Camthalion");
+       System.out.println("Ma devise est 'Parler peu mais parler bien'.");
+       
+ 
+    }
+ }
---
+++ Niveau 1/1/4-danslefourré.java
@@ -0,0 +1,18 @@
+import static algorea.Robot.*;
+ 
+class Main {
+   public static void main(String[] args) {
+      haut();
+      haut();
+      haut();
+      
+      droite();
+      droite();
+      
+      bas();
+      bas();
+      
+      droite();
+
+   }
+}
